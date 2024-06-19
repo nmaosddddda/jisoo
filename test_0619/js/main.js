4912a6b3517b77780c1b0f806b447c04db77f4e1
nmaosddddda <ajshi11@naver.com>
2024-06-19 17:34:22 +0900

    06.19

--- test_0619/js/main.js
+++ test_0619/js/main.js
@@ -1,25 +1,55 @@
 $(document).ready(function(){
-    const swiper = new Swiper('.swiper', { /* 팝업을 감싼는 요소의 class명 */
-
+    const swiper = new Swiper('.popup .swiper', { /* 팝업을 감싼는 요소의 class명 */
         autoplay: {  /* 팝업 자동 실행 */
             delay: 5000,
             disableOnInteraction: true,
         },
-        
         effect: "fade", /* fade 효과 */
-
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-
         pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-            el: '.swiper-pagination', /* 해당 요소의 class명 */
+            el: '.popup .swiper-pagination', /* 해당 요소의 class명 */
             clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
         },
-        
-
         navigation: {  /* 이전, 다음 버튼 */
-            nextEl: '.swiper-button-next',  /* 다음 버튼의 클래스명 */
-            prevEl: '.swiper-button-prev',  
+            nextEl: '.popup .swiper-button-next',  /* 다음 버튼의 클래스명 */
+            prevEl: '.popup .swiper-button-prev',  
         },
 
-    });
-})
+    });//swiper
+
+    
+const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+
+    autoplay: {  /* 팝업 자동 실행 */
+        delay: 5000,
+        disableOnInteraction: true,
+    },
+
+    loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+
+    pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+        el: '.visual .paging', /* 해당 요소의 class명 */
+        clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+    },
+    navigation: {  /* 이전, 다음 버튼 */
+        nextEl: '.visual .btn_next',  /* 다음 버튼의 클래스명 */
+        prevEl: '.visual .btn_prev',  
+    },
+
+    });//swiper
+
+    const book_swiper = new Swiper('.book .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: "auto", /* li의 넓이 비율로 안함 - css에서 준 넓이대로 함 */
+        spaceBetween: 16, /* li와 li사이 - 제일 작은 여백 */
+        breakpoints: {
+            640: {  /* 640px 이상이 되면 적용 */
+                spaceBetween: 24, 
+            },
+            1024: {  /* 1024px 이상이 되면 적용 */
+                spaceBetween: 24,
+            },
+        },
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+    });//swiper
+
+})//$(document).ready
