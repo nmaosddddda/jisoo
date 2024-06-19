c72bea0c255b8bfd4f8300fce2662472d8900c7d
nmaosddddda <ajshi11@naver.com>
2024-06-19 18:01:11 +0900

    .

--- test_0619/js/main.js
+++ test_0619/js/main.js
@@ -52,4 +52,36 @@ const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
     });//swiper
 
+    const sns_swiper = new Swiper('.sns .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            640: {    /* 640px 이상일때 적용 */
+                slidesPerView: 4,
+                spaceBetween: 24,
+            },
+        },
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+    });//swiper
+
+
+    /* 
+        .visual .stop {} -- 정지버튼
+        .visual .play {} -- 재생버튼
+        정지버튼을 클릭하면 visual_swiper 가 멈춤
+        재생버튼을 클릭하면 visual_swiper 가 autoplay
+    */
+    $(".visual .play").hide()
+    $(".visual .stop").on("click", function(){
+        visual_swiper.autoplay.stop(); /* 일시정지 기능 */
+        $(".visual .stop").hide()
+        $(".visual .play").show()	
+    });
+    $(".visual .play").on("click", function(){
+        visual_swiper.autoplay.start();  /* 재생 기능 */
+        $(".visual .stop").show()
+        $(".visual .play").hide()
+		
+    });
+
 })//$(document).ready
