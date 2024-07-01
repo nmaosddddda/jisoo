f6a08a26d3d0e2b3170360478079327f07050280
nmaosddddda <ajshi11@naver.com>
2024-07-01 13:47:28 +0900

    07.01.

--- animal/js/main.js
+++ animal/js/main.js
@@ -1,4 +1,25 @@
 $(document).ready(function(){
+
+    /* 브라우저 넓이가 1024px이하면 모바일 1024px이상이면 pc버전 */
+    let pc_mobile //현재 상태가 pc인지 모바일인지를 저장
+    let window_w //브라우저 넓이 저장 
+
+    function resize_chk(){
+        window_w = $(window).width()
+        if(window_w > 1024){ /* 1024보다 크면 - pc면 */
+             pc_mobile = 'pc'
+        }else{ /* mobile이면 */
+             pc_mobile = 'mobile'
+        }
+        console.log(pc_mobile)
+    }
+    //처음에 로딩 됐을때 실행
+    resize_chk()
+    //브라우저가 리사이즈 될때마다 실행
+    $(window).resize(function(){
+        resize_chk()
+    })
+
     const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
         autoplay: {  /* 팝업 자동 실행 */
@@ -60,6 +81,44 @@ $(document).ready(function(){
      $(window).scroll(function(){
         //브라우저를 스크롤 할때마다 실행
         scroll_chk()
-     })//window
+     })//window.scroll
+
+     /*
+         메뉴에 마우스를 올리면 
+         header에 menu_over라는 클래스를 추가
+         마우스를 오버한 li에만 on 이라는 클래스를 추가 
+         메뉴 : header .gnb .gnb_wrap ul.depth1 > li
+         (이전에 오버했던 메뉴를 알기 어려움)
+         ==> 모든 메뉴의 on클래스를 삭제했다가 오버한 내꺼만 on클래스 추가 
+         --> 다른 메뉴(li)에 마우스를 오버하면 
+             이전에 오버했던 메뉴의 on클래스 삭제
+        -->  header에서 마우스를 아웃하면 그때 menu_over라는 클래스 삭제
+             모든 메뉴에서 on클래스 삭제
+    */
+
+    $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseenter', function(){
+        if(pc_mobile == 'pc'){ //pc일 경우에만 
+            $('header').addClass('menu_over');
+            $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('on')
+            $(this).addClass('on') //마우스를 오버한 li만 
+        }//if
+    })//on
+    $('header').on('mouseleave', function(){
+        if(pc_mobile == 'pc'){ //pc일 경우에만
+            $('header').removeClass('menu_over');
+            $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('on')
+        }//if
+    })
+
+    $('header .gnb .gnb_open').on('click', function(){
+        if(pc_mobile == 'mobile'){ //mobile일 경우에만
+            $('header').addClass('mobile_open')
+        }
+    })
+    $('header .gnb .gnb_close').on('click', function(){
+        if(pc_mobile == 'mobile'){ //mobile일 경우에만
+            $('header').removeClass('mobile_open')
+        }
+    })
 
 })//document
