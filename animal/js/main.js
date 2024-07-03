a42392c735e9f99f9daf9d93ca5aa1cd64c894c9
nmaosddddda <ajshi11@naver.com>
2024-07-03 13:18:12 +0900

    07.03

--- animal/js/main.js
+++ animal/js/main.js
@@ -170,4 +170,27 @@ $(document).ready(function(){
     $(this).addClass('on')
    })
 
+   //family의 swiper
+
+   const family_swiper = new Swiper('.family .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 3, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            768: {    /* 768px 이상일때 적용 */
+                slidesPerView: 6,
+                spaceBetween: 5,
+            },
+            1300: {    /* 1300px 이상일때 적용 */
+                slidesPerView: 7,
+                spaceBetween: 5,
+            },
+        },
+        centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        navigation: {
+            nextEl: '.family .btn_wrap .next',
+            prevEl: '.family .btn_wrap .prev',
+        },
+    });//swiper
+
 })//document
