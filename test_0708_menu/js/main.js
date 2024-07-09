0150d8a893be847be345a41d94987d8d12c104b5
nmaosddddda <ajshi11@naver.com>
2024-07-09 10:34:50 +0900

    0709

--- test_0708_menu/js/main.js
+++ test_0708_menu/js/main.js
@@ -50,4 +50,36 @@ $(document).ready(function(){
         $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('on')
    }//if
   })
+
+  /* 
+     모바일에서 1차 메뉴를 클릭하면
+     - 1차메뉴에 걸려있는 링크가 작동되면 x
+     - 하위메뉴가 열려야함 (li에 open클래스 추가)
+       닫힌 1차 메뉴를 클릭하면 열리고 
+       열린 1차 메뉴를 클릭하면 닫힘
+       (동시에 여러개의 1차메뉴가 열릴 수 있음)
+  */
+     $("header .gnb .gnb_wrap ul.depth1 > li > a").on("click", function(e){
+          if(pc_mobile == 'mobile'){
+               e.preventDefault();		/* a 태그의 href를 작동 시키지 않음 */
+               $(this).parent().toggleClass('open')
+          }//if	
+	});//on
+
+     /*
+          메뉴 열기를 클릭하면 header에 menu_open 클래스 추가
+          메뉴 닫기를 클릭하면 header에 menu_open 클래스 삭제
+          header .gnb button.gnb_open - 메뉴열기
+          header .gnb button.gnb_close - 메뉴닫기
+     */
+    $('header .gnb button.gnb_open').on('click', function(){
+          $('header').addClass('menu_open')
+          $("html, body").css({overflow : "hidden", height : $(window).height()}).bind("scroll touchmove mousewheel", function(e){e.preventDefault();e.stopPropagation();return false;},function(){passive:false});
+    })
+    $('header .gnb button.gnb_close').on('click', function(){
+     $('header').removeClass('menu_open')
+     $("html, body").css({overflow : "visible", height : "auto"}).unbind('scroll touchmove mousewheel');
+})
+
+
 })
